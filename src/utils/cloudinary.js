167e2d7d4776bda3cff7bf7ec55282b6858107b1
AADESHak007 <123197632+AADESHak007@users.noreply.github.com>
2024-07-17 19:06:53 +0530

    setting up CLOUDINARY

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+
+//configuration
+cloudinary.config({ 
+    cloud_name: CLOUDINARY_CLOUD_NAME, 
+    api_key: CLOUDINARY_API_KEY, 
+    api_secret: CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadFile = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null ;
+       const res = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+       //after being successfully uploaded
+       console.log("uploaded successfully",res.url)
+       return res ;
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath) ;
+        return null ;
+    }
+}
+export {uploadFile} ;
